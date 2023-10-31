0c32738ab04834020b9ee2905798d7f792a06765
Dean Mercer <dean.j.mercer@gmail.com>
2023-10-31 16:21:50 +0100

    Add path calculations

--- src/path.cpp
+++ src/path.cpp
@@ -1,2 +1,57 @@
-#include <Eigen/Core>
+#include <Eigen/Dense>
 
+#include <limits>
+
+#include "path.h"
+
+using vec2 = Eigen::Vector2d;
+
+PathUtils::Path::Path(std::vector<vec2> points)
+:points_{points}
+{}
+
+double Distance(vec2 point_a, vec2 point_b){
+    return (point_b - point_a).norm();
+}
+
+
+PathIndex PathUtils::Path::FindClosestPointIndex(vec2 point){
+    double closest = std::numeric_limits<double>::max();
+    int closest_index = -1;
+    vec2 closest_point;
+
+    for(int i = 0; i < points_.size(); i++){
+        double distance = Distance(point, points_[i]);
+        if(distance < closest){
+            closest = distance;
+            closest_index = i;
+            closest_point = points_[i];
+        }
+    }
+
+    //TODO: find fraction
+
+    return PathIndex{closest_index, 0.0, closest_point};
+}
+
+        
+vec2 PathUtils::Path::FindPointAtDistance(vec2 point, double distance){
+    PathIndex start_index = FindClosestPointIndex(point);
+    double distance_to_start = Distance(point, start_index.point);
+    
+    if(distance <= distance_to_start){
+        return start_index.point;
+    }
+
+    double total_distance = distance_to_start;
+
+    for(int i = start_index.base_node; i < points_.size(); i++){
+        total_distance += Distance(point, points_[i]);
+        if(total_distance >= distance){
+            return points_[i];
+        }
+    }
+
+    // For the case that the desired length is longer than the path
+    return points_[points_.size()];
+}
--- src/path.h
+++ src/path.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <vector>
+#include <eigen3/Eigen/Dense>
+
+using vec2 = Eigen::Vector2d;
+
+struct PathIndex{
+    int base_node;
+    double fraction_to_next_node;
+    vec2 point;
+};
+
+namespace PathUtils{
+    class Path{
+    private:
+        std::vector<vec2> points_;
+
+        PathIndex FindClosestPointIndex(vec2 point);
+
+    public:
+        Path(std::vector<vec2> points);
+        vec2 FindPointAtDistance(vec2 point, double distance);
+
+    };
+}
